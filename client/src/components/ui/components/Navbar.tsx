082f5a1e2056c807e50a2a97bc2d5a170d2e8da0
Izzatbek <abdusharipovizzat03@gmail.com>
2026-01-14 01:24:15 +0500

    tasks

--- client/src/components/ui/components/Navbar.tsx
+++ client/src/components/ui/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 import { Input } from "../input"
 import { SidebarTrigger } from "../sidebar"
-import { BellDot, Moon } from "lucide-react"
+import { BellDot, LogOut, Moon, PanelTop, User, UsersRound } from "lucide-react"
+import { DropdownMenu, DropdownMenuSeparator, DropdownMenuTrigger } from "../dropdown-menu"
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@radix-ui/react-dropdown-menu"
+import { useState } from "react"
 
 const Navbar = () => {
+    const [menuOpen,setMenuOpen] = useState(false);
     return (
         <div className="flex items-center justify-between gap-4 sm:gap-6 md:gap-8 lg:gap-10 p-4">
             <div className="flex items-center gap-3">
@@ -14,10 +18,22 @@ const Navbar = () => {
                 <BellDot size={20} className="cursor-pointer text-gray-800" />
                 <Moon size={20} className="cursor-pointer text-gray-800" />
                 <p className="text-gray-400">|</p>
-                <Avatar className="cursor-pointer">
-                    <AvatarImage src="https://github.com/shadcn.png" className="w-8 rounded-full object-cover" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
+                <DropdownMenu open={menuOpen} onOpenChange={setMenuOpen}>
+                    <DropdownMenuTrigger asChild>
+                        <Avatar className="cursor-pointer" onClick={()=>setMenuOpen(true)}>
+                            <AvatarImage src="https://github.com/shadcn.png" className="w-8 rounded-full object-cover" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="bg-white p-4 shadow cursor-pointer m-4 space-y-2 w-40">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem className="flex items-center gap-2"><User size={20} />Profile</DropdownMenuItem>
+                        <DropdownMenuItem className="flex items-center gap-2"><PanelTop size={20} />Billing</DropdownMenuItem>
+                        <DropdownMenuItem className="flex items-center gap-2"><UsersRound size={20} /> Team</DropdownMenuItem>
+                        <DropdownMenuItem className="flex items-center gap-2"><LogOut size={20} /> Log out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     )
