e8df8b1e3bc0ea00dd6904592f31b632fd6156c9
Izzatbek <abdusharipovizzat03@gmail.com>
2026-01-14 20:52:13 +0500

    tasks

--- client/src/tasks/ui/components/task-form.tsx
+++ client/src/tasks/ui/components/task-form.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
 import { Textarea } from "@/components/ui/textarea"
-import { Tooltip } from "@/components/ui/tooltip"
-import { TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 import { Plus } from "lucide-react"
 
-const TaskForm = ({sheetOpen,setSheetOpen}:{sheetOpen:boolean,setSheetOpen:(val:boolean)=>void}) => {
+const TaskForm = ({ sheetOpen, setSheetOpen }: { sheetOpen: boolean, setSheetOpen: (val: boolean) => void }) => {
     return (
         <>
             <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
@@ -15,12 +15,12 @@ const TaskForm = ({sheetOpen,setSheetOpen}:{sheetOpen:boolean,setSheetOpen:(val:
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <Button className="rounded-full cursor-pointer shadow-md"
-                             onClick={()=>setSheetOpen(true)}>
+                                onClick={() => setSheetOpen(true)}>
                                 <Plus />
                             </Button>
                         </TooltipTrigger>
                         <TooltipContent>
-                            <p className="bg-white text-gray-900 text-sm">Add Task</p>
+                            <p>Add Task</p>
                         </TooltipContent>
                     </Tooltip>
                 </SheetTrigger>
@@ -29,13 +29,47 @@ const TaskForm = ({sheetOpen,setSheetOpen}:{sheetOpen:boolean,setSheetOpen:(val:
                         <SheetTitle>Add New Task </SheetTitle>
                     </SheetHeader>
                     <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                        {/* title */}
                         <div className="grid gap-3">
                             <Label htmlFor="title">Title</Label>
                             <Input id="title" defaultValue="Your task title" />
                         </div>
+                        {/* description */}
                         <div className="grid gap-3">
                             <Label htmlFor="description">Description</Label>
                             <Textarea id="description" defaultValue="Something content" />
+                        </div>
+                        {/* status */}
+                        <div className="grid gap-3">
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a status" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Status</SelectLabel>
+                                        <SelectItem value="TODO">Todo</SelectItem>
+                                        <SelectItem value="IN_PROGRESS">In progress</SelectItem>
+                                        <SelectItem value="DONW">Done</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        {/* priority */}
+                        <div className="grid gap-3">
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a priority" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Status</SelectLabel>
+                                        <SelectItem value="LOW">Low</SelectItem>
+                                        <SelectItem value="MEDIUM">Medium</SelectItem>
+                                        <SelectItem value="HIGH">High</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
                     <SheetFooter>
